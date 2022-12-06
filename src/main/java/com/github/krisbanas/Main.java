3bf238cb323beb9cae1eae47e6a05799c61b5e59
Krzysztof Banasik <krzysztof.banasik@protonmail.com>
2022-12-06 19:36:33 +0100

    Day 7

--- src/main/java/com/github/krisbanas/Main.java
+++ src/main/java/com/github/krisbanas/Main.java
@@ -1,18 +1,9 @@
 package com.github.krisbanas;
 
-import com.github.krisbanas.solutions.*;
+import com.github.krisbanas.solutions.Day07;
 
 public class Main {
     public static void main(String[] args) {
-        new Day06();
-    }
-
-    static void measurePerformance() {
-        new Day01();
-        new Day02();
-        new Day03();
-        new Day04();
-        new Day05();
-        new Day05();
+        new Day07();
     }
 }
---
+++ src/main/java/com/github/krisbanas/solutions/Day07.java
@@ -0,0 +1,20 @@
+package com.github.krisbanas.solutions;
+
+import static java.lang.System.out;
+
+public class Day07 {
+    public Day07() {
+        out.println(part1());
+        out.println(part2());
+    }
+
+    public Object part1() {
+        return null;
+    }
+
+    private Object part2() {
+
+        return null;
+    }
+}
+
