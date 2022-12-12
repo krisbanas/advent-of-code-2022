e738e5325211a22bddbe011ddc53c5b1d50fa1b1
Krzysztof Banasik <krzysztof.banasik@protonmail.com>
2022-12-12 19:59:39 +0100

    Template

---
+++ src/main/java/com/github/krisbanas/solutions/Day13.java
@@ -0,0 +1,17 @@
+package com.github.krisbanas.solutions;
+
+public class Day13 {
+
+    public Day13() {
+        System.out.println(part1());
+        System.out.println(part2());
+    }
+
+    public Object part1() {
+        return null;
+    }
+
+    public Object part2() {
+        return null;
+    }
+}
