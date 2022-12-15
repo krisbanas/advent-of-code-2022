3a46877097260088ee6b0d113ebf3db5164dc005
Krzysztof Banasik <krzysztof.banasik@protonmail.com>
2022-12-15 19:35:45 +0100

    Day 15

--- src/main/java/com/github/krisbanas/solutions/Day15.java
+++ src/main/java/com/github/krisbanas/solutions/Day15.java
@@ -1,13 +1,15 @@
 package com.github.krisbanas.solutions;
 
 
+import com.github.krisbanas.toolbox.Direction;
 import com.github.krisbanas.toolbox.FileReader;
+import com.github.krisbanas.toolbox.Point;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-
-import static com.github.krisbanas.toolbox.Extractor.ints;
+import java.util.stream.Collectors;
 
 public class Day15 {
 
@@ -17,63 +19,75 @@ public class Day15 {
     }
 
     public Object part1() {
-        var pairs = Arrays.stream(FileReader.readAsString("Day15Input.txt").trim().split("\n"))
-                .map(x -> x.split("[= ,:]"))
-                .map(x -> new Pair(new Point(Integer.parseInt(x[6]), Integer.parseInt(x[3])), new Point(Integer.parseInt(x[16]), Integer.parseInt(x[13])),
-                        getInfluence(new Point(Integer.parseInt(x[6]), Integer.parseInt(x[3])), new Point(Integer.parseInt(x[16]), Integer.parseInt(x[13])))))
-                .toList();
+        List<Pair> pairs = getPairs();
+        List<Point> takenPoints = getOccupiedPoints(pairs);
 
-        long left = pairs.stream().map(x -> List.of(x.sensor, x.bacon)).flatMap(Collection::stream).mapToLong(Point::col).min().getAsLong();
-        long right = pairs.stream().map(x -> List.of(x.sensor, x.bacon)).flatMap(Collection::stream).mapToLong(Point::col).max().getAsLong();
-        long top = pairs.stream().map(x -> List.of(x.sensor, x.bacon)).flatMap(Collection::stream).mapToLong(Point::row).min().getAsLong();
-        long bot = pairs.stream().map(x -> List.of(x.sensor, x.bacon)).flatMap(Collection::stream).mapToLong(Point::row).max().getAsLong();
-
-        var takenPoints = pairs.stream().map(x -> List.of(x.sensor, x.bacon)).flatMap(Collection::stream).toList();
+        int left = pairs.stream().map(x -> List.of(x.sensor, x.bacon)).flatMap(Collection::stream).mapToInt(Point::col).min().getAsInt();
+        int right = pairs.stream().map(x -> List.of(x.sensor, x.bacon)).flatMap(Collection::stream).mapToInt(Point::col).max().getAsInt();
 
         int count = 0;
-//
-//        long OFFSET = 100000;
-//        for (long i = left - OFFSET; i <= right + OFFSET; i++) {
-//            Point pointer = new Point(2000000, i);
-//            if (takenPoints.contains(pointer)) continue;
-//            if (pairs.stream().anyMatch(x -> isBlockedBy(pointer, x))) {
-//                count++;
-//            }
-//            else {
-//                System.out.println("no");
-//            }
-//        }
-
-
-        long OFFSET = 10;
-        for (long i = left - 10; i <= right + 10; i++) {
-            Point pointer = new Point(10, i);
+        for (int i = left - 1000000; i <= right + 1000000; i++) {
+            Point pointer = new Point(2000000, i);
             if (takenPoints.contains(pointer)) continue;
-            if (pairs.stream().anyMatch(x -> isBlockedBy(pointer, x))) count++;
-        }
-
-
-        for (long i = top - OFFSET; i < bot + OFFSET; i++) {
-            System.out.printf("%2d ", i);
-
-            for (long j = left - OFFSET; j < right + OFFSET; j++) {
-                Point p = new Point(i, j);
-                if (takenPoints.contains(p)) System.out.print("B");
-                else if (pairs.stream().anyMatch(x -> isBlockedBy(p, x))) System.out.print("#");
-                else if (isBlockedBy(p, new Pair(new Point(7, 8), new Point(10, 2), getInfluence(new Point(7, 8), new Point(10, 2))))) System.out.print("#");
-                else System.out.print(".");
+            if (pairs.stream().anyMatch(x -> isBlockedBy(pointer, x))) {
+                count++;
             }
-            System.out.println();
         }
 
         return count;
     }
 
     public Object part2() {
-        return null;
+        List<Pair> pairs = getPairs();
+        List<Point> takenPoints = getOccupiedPoints(pairs);
+
+        var edges = pairs.stream().map(Pair::getEdge).flatMap(Collection::stream).collect(Collectors.toSet());
+
+        for (Point p : edges) {
+            if (p.row() < 0 || p.row() > 4000000 || p.col() < 0 || p.col() > 4000000) continue;
+            if (takenPoints.contains(p)) continue;
+            if (pairs.stream().anyMatch(x -> isBlockedBy(p, x))) continue;
+            return 4000000L * p.col() + p.row();
+        }
+        return 0;
+    }
+
+    private static List<Pair> getPairs() {
+        return Arrays.stream(FileReader.readAsString("Day15Input.txt").trim().split("\n"))
+                .map(x -> x.split("[= ,:]"))
+                .map(x -> new Pair(new Point(Integer.parseInt(x[6]), Integer.parseInt(x[3])), new Point(Integer.parseInt(x[16]), Integer.parseInt(x[13])),
+                        getInfluence(new Point(Integer.parseInt(x[6]), Integer.parseInt(x[3])), new Point(Integer.parseInt(x[16]), Integer.parseInt(x[13])))))
+                .toList();
     }
 
     record Pair(Point sensor, Point bacon, double influence) {
+        List<Point> getEdge() {
+            List<Point> result = new ArrayList<>();
+            Point pointer = Direction.EAST.move(sensor, (int) (influence - 0.000000001) + 2);
+            result.add(pointer);
+            while (pointer.row() > sensor.row()) {
+                pointer = Direction.NORTHWEST.move(pointer);
+                result.add(pointer);
+            }
+            while (pointer.col() < sensor.col()) {
+                pointer = Direction.SOUTHWEST.move(pointer);
+                result.add(pointer);
+            }
+            while (pointer.row() < sensor.row()) {
+                pointer = Direction.SOUTHEAST.move(pointer);
+                result.add(pointer);
+            }
+            while (pointer.col() > sensor.col()) {
+                pointer = Direction.NORTHEAST.move(pointer);
+                result.add(pointer);
+            }
+
+            return result;
+        }
+    }
+
+    private static List<Point> getOccupiedPoints(List<Pair> pairs) {
+        return pairs.stream().map(x -> List.of(x.sensor, x.bacon)).flatMap(Collection::stream).toList();
     }
 
     private boolean isBlockedBy(Point test, Pair pair) {
@@ -84,9 +98,4 @@ public class Day15 {
     public static double getInfluence(Point a, Point b) {
         return (Math.abs(a.row() - b.row()) + Math.abs(a.col() - b.col()));
     }
-
-    record Point(long row, long col) {
-
-    }
 }
-
