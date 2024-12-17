850915799d7fc431de56e57266a2c4f87e2249f5
Krzysztof Banasik <krzysztof.f.banasik@gmail.com>
2024-12-17 21:55:19 +0100

    Update

--- src/main/java/com/github/krisbanas/Main.java
+++ src/main/java/com/github/krisbanas/Main.java
@@ -1,9 +1,9 @@
 package com.github.krisbanas;
 
-import com.github.krisbanas.solutions.Day17;
+import com.github.krisbanas.solutions.Day18;
 
 public class Main {
     public static void main(String[] args) {
-        new Day17();
+        new Day18();
     }
 }
---
+++ src/main/java/com/github/krisbanas/solutions/Day18.java
@@ -0,0 +1,17 @@
+package com.github.krisbanas.solutions;
+
+public class Day18 {
+
+    public Day18() {
+        System.out.println(part1());
+        System.out.println(part2());
+    }
+
+    public Object part1() {
+        return null;
+    }
+
+    public Object part2() {
+        return null;
+    }
+}
